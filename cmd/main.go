a41a6146cc099a84fbbc6803a33405df5635033f
Sebastian Nunez <02snunez@gmail.com>
2024-12-25 23:14:32 -0500

    feat: add Fiber package with ready, live endpoints, logging and a basic HTTP server

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/sebastian-nunez/golang-search-engine/config"
 )
 
 func main() {
-	port := ":" + config.Envs.Port
-	fmt.Println("Port -> " + port)
+	app := fiber.New()
+
+	app.Use(logger.New())
+	app.Use(healthcheck.New(healthcheck.Config{
+		LivenessEndpoint:  "/live",
+		ReadinessEndpoint: "/ready",
+	}))
+	app.Get("/metrics", monitor.New())
+
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+	v1.Get("/", func(c *fiber.Ctx) error {
+		log.Info("Getting data!")
+		return c.SendString("Hello world!")
+	})
+
+	app.Listen(":" + config.Envs.Port)
 }
