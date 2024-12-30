129e9990224f8a0f7f933b128a79eb145b5a1201
Sebastian Nunez <02snunez@gmail.com>
2024-12-29 23:56:51 -0500

    chore: adds some better comments

--- core/indexer.go
+++ core/indexer.go
@@ -6,9 +6,23 @@ import (
 	"github.com/sebastian-nunez/golang-search-engine/db"
 )
 
-// InvertedIndex is an in-memory inverted index. It maps tokens to a set of page IDs (stored in the database).
+// InvertedIndex is a map that stores tokens (words) as keys and maps each token
+// to a set of associated page IDs. This structure allows for efficient lookups
+// of pages containing specific tokens.
+//
+// Example:
+//
+//	index := InvertedIndex{
+//	    "hello": {"page1": struct{}{}, "page3": struct{}{}},
+//	    "world": {"page2": struct{}{}, "page3": struct{}{}},
+//	}
+//
+// This example shows that the token "hello" appears on pages "page1" and "page3",
+// while the token "world" appears on pages "page2" and "page3".
 type InvertedIndex map[string]map[string]struct{}
 
+// Add adds crawled pages to the inverted index (in-memory), efficiently handling
+// duplicate page entries for the same token.
 func (idx InvertedIndex) Add(pages []db.CrawledPage) {
 	for _, page := range pages {
 		doc := buildDocument(page)
